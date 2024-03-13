851c37b9c54e438c152253da3a560ac2f9b2855b
agualyx <778565515@qq.com>
2024-03-13 23:31:20 +0800

    asset/source 学习完成

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a Readme file.
+## 笔记
+使用asset/source匹配txt文件，会将txt文件中的文字编译成字符串，可以在js中直接导入该txt文件使用其字符串
--- src/add-image.js
+++ src/add-image.js
@@ -1,9 +1,10 @@
 import Kiwi from './kiwi.jpg';
+import altStr from './altStr.txt';
 
 function addImage() {
     // Inside this function I will create an img dom element, specify an alt , width , and src properties.
     const img = document.createElement('img');
-    img.alt = 'Kiwi';
+    img.alt = altStr;
     img.width = 300;
     img.src = Kiwi;
     const body = document.querySelector('body');
